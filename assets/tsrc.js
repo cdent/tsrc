221c7ca2debace0682235dba876a6caeea912c39
Chris Dent <chris.dent@gmail.com>
2013-11-03 18:31:11 +0000

    Add SiteIcon and link and some style.
    Some details:
    
    * it only shows SiteIcon's if it thinks the space multiple
      members (that is the space name is different from the names of the
      members)
    * the style is the usual meandering bs from me, only reason it has
      any hope is because I borrowed much of it from @colmjude

--- assets/tsrc.js
+++ assets/tsrc.js
@@ -14,9 +14,20 @@ app.filter('modtime', function() {
 app.controller('ChangesCtrl', ['$scope', '$http', function($scope, $http) {
 
 	var space = tiddlyweb.status.space.name,
+		server_host = tiddlyweb.status.server_host.host,
+		scheme = tiddlyweb.status.server_host.scheme,
+		port = tiddlyweb.status.server_host.port,
+		port = (port == 80 || port == 443) ? '' : port,
 		spaceQuery = '(bag:' + space + '_public%20OR%20bag:'
 			+ space + '_private)',
 		userQuery = 'modifier:' + space;
+
+	$scope.modifierSite = function(change) {
+		// XXX: if the oldest tiddlers is modidifed by the space name
+		// this trick won't work.
+		$scope.multiplemods = !(change.modifier == space);
+		return scheme + '://' + change.modifier + '.' + server_host + '/';
+	}
 
 	$scope.$parent.space = space;
 	$scope.show = '';
